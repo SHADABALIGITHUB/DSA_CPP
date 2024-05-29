082f42c3fcee820a8cf670602975eb96fafb4d4b
shadab <shad46243@gmail.com>
2024-05-30 05:02:49 +0530

    dynamic array pointer

---
+++ pointer/04_create_dynamic_array_pointing.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+                    
+    int a[]={12,23,3,44,5,56,67};
+    int *p=a;
+
+    for(int i=0;i<6;i++){
+        cout<<(a+i)<<endl;
+    }
+                 
+    return 0;
+}
