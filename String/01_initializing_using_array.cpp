d51e89ccfb1eeb7cbbf72728527aa62e98799351
shadab <shad46243@gmail.com>
2024-05-30 21:29:27 +0530

    String folder creation

---
+++ String/01_initializing_using_array.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+                    
+     char str[]={'a','b','c','d','e','\0'};
+
+     char str2[6]="Hello";
+
+      cout<<str<<endl;
+
+      cout<<str2<<endl;
+
+                 
+    return 0;
+}
--- pointer/04_create_dynamic_array_pointing.cpp
+++ pointer/04_create_dynamic_array_pointing.cpp
@@ -3,10 +3,10 @@ using namespace std;
 int main(){
                     
     int a[]={12,23,3,44,5,56,67};
-    int *p=a;
+   
 
     for(int i=0;i<6;i++){
-        cout<<(a+i)<<endl;
+        cout<<a<<endl;
     }
                  
     return 0;
