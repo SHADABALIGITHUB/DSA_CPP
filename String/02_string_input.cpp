c9e149541a2684562bf1ba9e1103e02080a25081
shadab <shad46243@gmail.com>
2024-05-31 19:36:49 +0530

    String input

---
+++ String/02_string_input.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+     char str[50];
+    //   print only first word not the whole string stop at space
+    //     cout<<"Enter a string: "<<endl;
+    //   cin>>str;
+    //   cout<<str<<endl;
+
+    //   print the whole string
+    cout<<"Enter a string: "<<endl;
+    cin.getline(str,50);
+    cout<<str<<endl;
+
+
+
+   
+      
+                 
+    return 0;
+}
