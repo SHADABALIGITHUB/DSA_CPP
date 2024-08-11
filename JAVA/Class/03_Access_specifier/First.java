b24409101894c748792de11a18ae0d69705a8e77
shadab <shad46243@gmail.com>
2024-08-11 23:42:27 +0530

    Access specifier public

---
+++ JAVA/Class/03_Access_specifier/First.java
@@ -0,0 +1,19 @@
+public class First{
+     public int a=20;
+     int b=10;
+
+     public void show(){
+        System.out.println("Value of Show is :" + a);
+     }
+
+
+
+    public static void main(String args[]){
+
+              First ob=new First();
+              ob.show();
+
+ 
+    }
+    
+}
