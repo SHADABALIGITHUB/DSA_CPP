71e3c97ea8f7b415841d4283c215e067e6ebd992
shadab <shad46243@gmail.com>
2024-10-02 19:43:45 +0530

    09 chapter apna college Done c++ series

---
+++ CPP/Apnacollege/chapter09/index.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+//      1. declare 
+    //   vector<int> vec;
+
+
+    //   2 with 5 values each 0 
+    //  vector<int> vec(5,0);
+
+    //    3 with value -1
+    //    vector<int> vec(8,-1);
+
+    //  4   direct values into vector 
+    //   vec={1,2,3,4,5,6,7};
+
+       
+    //    5  other data types too
+    //   vector<char> vec={'a','b','y'};
+    //  vector<bool> vec={false,false,true};
+
+
+    //    6 update hole array or change its values 
+    //  vector<int> vec={1,2,3,4,5,6,7};
+    //   vec={11,12,13,14,15,16,17};
+
+
+    //  for(int i=0;i<vec.size();i++){
+
+    //     cout<<vec[i]<<endl;
+    //  }
+
+
+
+     
+
+
+
+
+    //       functions of vector 
+
+    //  1.  push_back 2.  pop_back  3. back()  4. front()   5.) at  6.)   size  7.)  capacity 
+
+
+
+      
+
+//     leet code problem   unique value i solev it using xor 
+
+    return 0;
+}
